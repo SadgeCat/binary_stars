4986fddf852e4c2eff24fd14e65dea18243486fe
Yuhang Pan <YuchenPan@MacBookAir.fios-router.home>
2026-05-22 20:36:18 -0400

    somewhat working equipotential

--- program.py
+++ program.py
@@ -15,7 +15,7 @@ dist = 1e11
 x1 = -dist*mB/(mA+mB)
 x2 = dist*mA/(mA+mB)
 
-lobe_rad = 
+#lobe_rad = 
 
 # 2 stars for binary star system
 starA = sphere(pos = vector(x1, 0, 0), radius = 2.7*Rs, color = color.yellow)
@@ -38,13 +38,15 @@ def gravity(star, satellite):
     rad = satellite.pos - star.pos
     return -G*star.mass*satellite.mass*hat(rad)/(mag(rad)**2)
     
+
+sep_dist = mag(starA.pos - starB.pos)
 def potential(x, y, z):
-    sep_dist = starA.pos - starB.pos
+#    sep_dist = starA.pos - starB.pos
     w_squared = G * starA.mass * (1 + q)/(sep_dist ** 3)
     
-    r1 = ((starA.pos.x - x) ** 2 + y ** 2 + z ** 2) ** (1/2)
-    r2 = ((starB.pos.x - x) ** 2 + y ** 2 + z ** 2) ** (1/2)
-    r3_squared = (x**2 + y**2 + z**2)
+    r1 = sqrt((starA.pos.x - x) ** 2 + y ** 2 + z ** 2)
+    r2 = sqrt((starB.pos.x - x) ** 2 + y ** 2 + z ** 2)
+    r3_squared = x**2 + y**2 + z**2
     
 #    W = 1/r1 + 1/r2 + 0.5*(1+q)*x1**2
     W = G*mA/r1 + G*mB/r2 + 0.5*w_squared*r3_squared
@@ -55,8 +57,8 @@ def potential(x, y, z):
 best_x = 0
 least_force = 1e100
 
-for i in range(100):
-    x = x1 + (x2-x1)*i/100
+for i in range(200):
+    x = x1 + (x2-x1)*i/200
     r1 = abs(x1-x)
     r2 = abs(x2-x)
     w_squared = G * starA.mass * (1 + q)/(sep_dist ** 3)
@@ -66,8 +68,23 @@ for i in range(100):
         least_force = f
         best_x = x
         
-equipotential = potential(best_x, 0)
+equipotential = potential(best_x, 0, 0)
     
+# calculate points close enough to the value of equipotential
+grid_size = 2 * dist
+step = 2e9
+pts_list = []
+for x in arange(-grid_size, grid_size, step):
+    for y in arange(-grid_size, grid_size, step):
+        W = potential(x, y, 0)
+        if abs(W - equipotential) < abs(equipotential)*1e-2:
+            pts_list.append(vector(x,y,0))
+
+for p in pts_list:
+    print(p)
+    sphere(pos = p, radius = 1.2e9, color = color.white)
+
+print("done drawing equipotential")
     
 t=0; dt=3600
     
